5d2efebebac1d4cdb49e4b0abab51245d656963a
Breyiner <breyneracosta7@gmail.com>
2026-01-13 16:25:09 -0500

    se implementa el componente de selector de roles en el header

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -1,9 +1,14 @@
+import RoleSelector from "../RoleSelector/RoleSelector";
 import "./AppHeader.css";
 
 export default function AppHeader({title}) {
   return (
     <header className="app-header">
       <h1 className="app-header__title">{title}</h1>
+
+      <div className="app-header__actions">
+        <RoleSelector />
+      </div>
     </header>
   );
 }
