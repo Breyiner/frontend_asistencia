bb36472e41ac46972dea17e257e82eacdd6e57b9
Breyiner <breyneracosta7@gmail.com>
2026-01-13 11:56:30 -0500

    se implementa proteccion para las rutas dentro de la app y ademas se agregan contenidos temporales para visualizacion en desarrollo

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -2,8 +2,29 @@ import { createBrowserRouter, Navigate } from "react-router-dom";
 
 import LoginFormPage from "../pages/AuthPages/LoginFormPage";
 import RegisterFormPage from "../pages/AuthPages/RegisterFormPage";
+import ProtectedRoute from "../components/ProtectedRoute/ProtectedRoute";
 import UnauthorizedPage from "../pages/UnauthorizedPage/UnauthorizedPage";
+import AppLayout from "../layouts/AppLayout/AppLayout";
 import AuthLayout from "../layouts/AuthLayout/AuthLayout";
+
+function HomePage() {
+  return (
+    <div style={{ padding: "24px", textAlign: "center" }}>
+      <h2>¡Layout funcionando!</h2>
+      <p>Sidebar y Header se ven perfectos aquí.</p>
+    </div>
+  );
+}
+
+function ProfilePage() {
+  return (
+    <div style={{ padding: "24px", textAlign: "center" }}>
+      <h2>Bienvenido al tu Perfil</h2>
+    </div>
+  );
+}
+
+
 
 export const router = createBrowserRouter([
     // AUTH (público)
@@ -13,6 +34,21 @@ export const router = createBrowserRouter([
             { path: "/", element: <Navigate to="/login" replace /> },
             { path: "/login", element: <LoginFormPage /> },
             { path: "/register", element: <RegisterFormPage /> },
+        ],
+    },
+
+    // APP (protegido)
+    {
+        element: <ProtectedRoute />,
+        children: [
+            {
+                element: <AppLayout />,
+                children: [
+                    { path: "/home", element: <HomePage /> },
+                    { path: "/users", element: <HomePage /> },
+                    { path: "/profile", element: <ProfilePage /> },
+                ],
+            },
         ],
     },
 
