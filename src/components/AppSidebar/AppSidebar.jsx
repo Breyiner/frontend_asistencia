5cb8f4694dc7950c43c18f63e73e96b59814af56
Breyiner <breyneracosta7@gmail.com>
2026-01-13 16:23:18 -0500

    se implementa el hook para manejar las rutas interactivas

--- src/components/AppSidebar/AppSidebar.jsx
+++ src/components/AppSidebar/AppSidebar.jsx
@@ -1,40 +1,33 @@
 import { NavLink } from "react-router-dom";
-import { RiDashboardLine, RiUser3Line } from "@remixicon/react";
-import "./AppSidebar.css";
 import UserProfile from "../UserProfile/UserProfile";
+import { useAuthMenu } from "../../hooks/useAuthMenu";
+import "./AppSidebar.css";
 
 export default function AppSidebar() {
+  const menuItems = useAuthMenu();
+
   return (
     <aside className="app-sidebar">
       <div className="app-sidebar__brand">
         <div className="app-sidebar__logo">
           <img src="/logo_sena_white.png" alt="Logo SENA" />
         </div>
-        <span className="app-sidebar__title">
-          Asistencia <br></br>SENA
-        </span>
+        <span className="app-sidebar__title">Asistencia</span>
       </div>
 
       <nav className="app-sidebar__nav">
-        <NavLink
-          to="/home"
-          className={({ isActive }) =>
-            `app-sidebar__item ${isActive ? "app-sidebar__item--active" : ""}`
-          }
-        >
-          <RiDashboardLine className="app-sidebar__icon" />
-          Dashboard
-        </NavLink>
-
-        <NavLink
-          to="/users"
-          className={({ isActive }) =>
-            `app-sidebar__item ${isActive ? "app-sidebar__item--active" : ""}`
-          }
-        >
-          <RiUser3Line className="app-sidebar__icon" />
-          <span>Usuarios</span>
-        </NavLink>
+        {menuItems.map(item => (
+          <NavLink 
+            key={item.to}
+            to={item.to} 
+            className={({ isActive }) => 
+              `app-sidebar__item ${isActive ? 'app-sidebar__item--active' : ''}`
+            }
+          >
+            <item.icon className="app-sidebar__icon" />
+            <span>{item.label}</span>
+          </NavLink>
+        ))}
       </nav>
 
       <UserProfile />
