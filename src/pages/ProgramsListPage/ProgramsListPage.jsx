6280e6a4e2ef41d74f1c0fa26e45bc6e8fc04cf7
Breyiner <breyneracosta7@gmail.com>
2026-01-16 11:56:12 -0500

    se implementa la ruta de listar programas de formación

---
+++ src/pages/ProgramsListPage/ProgramsListPage.jsx
@@ -0,0 +1,50 @@
+import DataListLayout from "../../components/DataList/DataListLayout";
+import "../../components/Badge/Badge.css";
+
+export default function ProgramsListPage() {
+  return (
+    <DataListLayout
+      title="Listado de Programas de Formación"
+      endpoint="training_programs"
+      createPath="/training_programs/create"
+      initialFilters={{ per_page: 10 }}
+      rowClickPath={(p) => `/training_programs/${p.id}`}
+      filtersConfig={[
+        {
+          name: "program_name",
+          label: "Nombre",
+          placeholder: "Nombre",
+          defaultValue: "",
+          withSearchIcon: true,
+        },
+        {
+          name: "area_name",
+          label: "Área",
+          placeholder: "Área",
+          defaultValue: "",
+        },
+        {
+          name: "qualification_level_name",
+          label: "Nivel",
+          placeholder: "Nivel",
+          defaultValue: "",
+        },
+      ]}
+      tableColumns={[
+        { key: "name", label: "Nombre"},
+        { key: "area_name", label: "Área" },
+        { key: "fichas_count", label: "Fichas Relacionadas" },
+        { key: "duration", label: "Duración" },
+        { 
+            key: "qualification_level_name", 
+            label: "Nivel",
+            render: (p) => (
+            <span className={'badge'}>
+              {p.qualification_level_name}
+            </span>
+          ),
+        },
+      ]}
+    />
+  );
+}
